047a6bb9d6c9c65c0316bf136b742dc9a8239a03
nj4funn@gmail.com <nj4funn@gmail.com>
2024-01-07 16:22:40 +0100

    First Commit

---
+++ src/destacados.js
@@ -0,0 +1,55 @@
+
+import foto16 from './Assets/Estampados/Pandito.png'
+import foto from './Retratos/Retrato_11.png'
+import foto2 from './Assets/Estampados/Ninja_cat.png'
+import foto3 from './Retratos/Retrato_4.png'
+
+
+
+
+export const destacados = [
+    {
+        id: "160",
+        titulo:"Genius Partying",
+        foto: foto,
+        categoria:"retratos",
+        subcategoria:"historia",
+        price: 12,
+        inicial: 'negro',
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit hac scelerisque cum, eros arcu semper fusce conubia proin laoreet at sodales, vivamus blandit massa ultrices vestibulum posuere purus feugiat bibendum. Cras urna semper posuere volutpat luctus conubia pretium co"
+    },
+
+    {
+        id: "26",
+        titulo:"Brown Bear",
+        categoria:"animales",
+        subcategoria:"Bear",
+        foto: foto16,
+        price: 14,
+        inicial: 'blanco',
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit hac scelerisque cum, eros arcu semper fusce conubia proin laoreet at sodales, vivamus blandit massa ultrices vestibulum posuere purus feugiat bibendum. Cras urna semper posuere volutpat luctus conubia pretium co"
+    },
+
+    {
+        id: "8",
+        titulo:"Leones",
+        foto: foto2,
+        categoria:"animales",
+        subcategoria:"Leones",
+        price: 12,
+        inicial: 'negro',
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit hac scelerisque cum, eros arcu semper fusce conubia proin laoreet at sodales, vivamus blandit massa ultrices vestibulum posuere purus feugiat bibendum. Cras urna semper posuere volutpat luctus conubia pretium co"
+    },
+
+    {
+        id: "153",
+        titulo:"Vini Vidi Vicci",
+        foto: foto3,
+        categoria:"retratos",
+        subcategoria:"retratos",
+        price: 12,
+        inicial: 'blanco',
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit hac scelerisque cum, eros arcu semper fusce conubia proin laoreet at sodales, vivamus blandit massa ultrices vestibulum posuere purus feugiat bibendum. Cras urna semper posuere volutpat luctus conubia pretium co"
+    },
+
+]
